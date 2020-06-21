017967833345466f4d1e00b34f53e42b7d404bee
Bilal-Hanif01 <hanifbilal03@gmail.com>
2020-06-22 04:37:59 +0500

    JavaScriptAssignment

---
+++ chap14to16.js
@@ -0,0 +1,84 @@
+
+
+// // 1. Declare an empty array using JS literal notation to store
+// // student names in future.
+// var Student ={Name:"Bilal",Age:"24"}     
+// (Student)
+
+// // 2. Declare an empty array using JS object notation to store
+// // student names in future.
+
+// var Student ={
+//     Name:"Bilal",
+//     Age:"24"
+//      (Student)
+// }
+
+// 3. Declare and initialize a strings array.
+
+// var Student ={"Name":"Bilal","Age":"24"}     
+// console.log(Student)
+// // // 4. Declare and initialize a strings array.
+// // var Student =[1,2,3,4]     
+// // console.log(Student)
+
+// var faculty=["1) SSC","2)HSC","3) BSC","4) BS"]
+// document.write("<h1>Qualification</h1>")
+// document.write(faculty[0],"<br>");
+// document.write(faculty[1],"<br>");
+// document.write(faculty[2],"<br>");
+// document.write(faculty[3],"<br");
+// document.write(faculty[4],"<br>");
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scoress & percentages of students like:
+
+//  var f=["Micheal","John","Tony"]
+//  var s=[320,230,480] 
+// for(i=0;i<s.length;i++){
+//     var p=(s[i]/500)*100;
+//     document.write("Score 0f "+f[i],"is"+s[i]+"Percentage of"+p+"%"+"<br>")
+// }
+
+
+ 
+// 10. Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+// var s =[950,850,760,650,550,404]
+// document.write("score of Student:",s,"<br>")
+// document.write("order score of Student :",s[5],s[4],s[3],s[2],s[1],s[0])
+
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+//  var cities=["karachi","Islamabad","lahore","peshawar","queta",",multan"]
+//  document.write(cities)
+
+    
+//      if(cities=="karachi","islamabad","lahore"){
+//         document.write("<br>"+"Selected cities list"+"<br>",cities[0],cities[1],cities[2])
+//      }
+// // 2. Write a program to create a single string from the
+// // below mentioned array:
+// // var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// // (Use array’s join method)
+// var g =["this","is","my" ,"cat"]
+// document.write("<h1>Array</h1>",g)
+//  document.write("<h1>String</h1>",g[0]," ",g[1]," ", g[2]," ",g[3])
+
+// 3. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they
+// were stored. (FIFO-First In First Ou
+// document.write("<h1>Devices</h1>")
+// var a =["keybord","monitor","mouse","printer"]
+// for(var i =0;i<a.length;i++){
+//     document.write("<h1>out</h1>",a[i],"<br>")
+// // }
+
+
+
+
---
+++ chap17to20.js
@@ -0,0 +1,28 @@
+
+
+
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+// var  a =["name","dsfwerl","asdfwf",
+//           24   , 34555   ,  3655.,]
+// 2. Declare and initialize an empty multidimensional array.
+// var a =[0,1,2,3,
+//         3,2,1,9,
+//         8,4,5,3,]
+// // document.write(a)        
+
+// 3. Write a program to print numeric counting from 1 to 10
+// for(i=1;i<=10;i++){
+//     document.write(i,"<br>")
+
+// var a =prompt("Enter Your Table Number");
+// var b =prompt("Enter your table lenght");
+
+// for (i = 1 ;i<=b.length;i++){
+
+// document.write(a+" "+ "x"+"" +i+"=");
+// document.write(2*i+"<br>");
+// }
+
+
+/
